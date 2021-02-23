1a2ec77bf0dffe9adff6614461acc5abc13b9b11
asgavane <asgavane@ncsu.edu>
2021-02-22 19:01:41 -0500

    init newsfeed and stats sections

---
+++ src/components/Newsfeed.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/Newsfeed.css"
+
+function Newsfeed() {
+    return (
+        <div className="newsfeed">
+            <div className="newsfeed__container">
+                <h1>Newsfeed</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Newsfeed
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import { View } from 'react-native'
 import Header from "../components/Header"
+import Newsfeed from '../components/Newsfeed'
+import Stats from '../components/Stats'
 import "../styles/HomeScreen.css"
 
 function HomeScreen() {
     return (
         <View style={{flex: 1, backgroundColor: 'black'}}>
-            <div className="homescreen">
+            <div className="app">
                 {/* Header */}
-                <div className="homescreen__header">
+                <div className="app__header">
                     <Header />
                 </div>
                 {/* Body */}
-                <div className="homescreen__body">
-                    
+                <div className="app__body">
+                    <div className="app__container">
+                        <Newsfeed />
+                        <Stats />
+                    </div>
                 </div>
             </div>
         </View>
