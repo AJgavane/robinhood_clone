21ff3f10550b9b6c7c0fdb86bfde786fbf03be4f
asgavane <asgavane@ncsu.edu>
2021-02-23 16:15:06 -0500

    added portfolio chart

--- src/components/Newsfeed.js
+++ src/components/Newsfeed.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import "../styles/Newsfeed.css"
+import LineGraph from './LineGraph'
 
 function Newsfeed() {
     return (
         <div className="newsfeed">
             <div className="newsfeed__container">
-                <h1>Newsfeed</h1>
+                {/* Chart -- portfolio value and graph */}
+                <div className="newsfeed__chartSection">
+                    {/* Porfolio value */}
+                    <div className="newsfeed__portfolio">
+                        <h1>$2,908.88</h1>
+                        <p>+$373.4 (+14.72%) All time</p>
+                    </div>
+                    {/* Portfolio chart */}
+                    <div className="newsfeed__chart">
+                        <LineGraph />
+                    </div>
+                </div>
+                {/* Buying Power */}
+                {/* Market News */}
+                {/* Popular lists */}
             </div>
         </div>
     )
